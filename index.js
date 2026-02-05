726ac8bacc8251fc78cd87cee35810692936f47a
ABDUR-RAHIM1 <abdurrahim88557@gmail.com>
2026-02-05 17:56:53 +0600

    frontend url change

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.use(express.json());
 // app.use(cors());
 app.use(
     cors({
-        origin: "http://localhost:3000", // frontend URL
+        origin: "https://ainsohayok.vercel.app/", // frontend URL
+        // origin: "http://localhost:3000", // frontend URL
         credentials: true,
     })
 );
