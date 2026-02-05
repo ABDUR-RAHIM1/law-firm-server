6287bc4a50a89fc1c00bf41b85b0c1d658b4c46a
ABDUR-RAHIM1 <abdurrahim88557@gmail.com>
2026-02-05 18:06:17 +0600

    cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
     cors({
-        origin: "https://ainsohayok.vercel.app/", // frontend URL
+        origin: "https://ainsohayok.vercel.app", // frontend URL
         // origin: "http://localhost:3000", // frontend URL
         credentials: true,
     })
